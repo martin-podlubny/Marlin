d21965abe22f123a77b455b0643e659ba030f040
Martin Podlubny <martin.podlubny@gmail.com>
2021-12-09 07:37:36 +0000

    decrease the max bed temp to protect magnets

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -355,7 +355,7 @@
 #define HEATER_2_MAXTEMP 275
 #define HEATER_3_MAXTEMP 275
 #define HEATER_4_MAXTEMP 275
-#define BED_MAXTEMP 100
+#define BED_MAXTEMP 75
 
 //===========================================================================
 //============================= PID Settings ================================
